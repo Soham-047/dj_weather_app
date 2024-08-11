e7dbdaea1d7d2db7050b695710a801bcb79062e7
Soham-047 <129654959+Soham-047@users.noreply.github.com>
2024-08-12 02:19:04 +0530

    ii

--- weather/wsgi.py
+++ weather/wsgi.py
@@ -7,11 +7,21 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
-
+import os, django
+import sys
 from django.core.wsgi import get_wsgi_application
 
+path = '/home/yourusername/repository'
+if path not in sys.path:
+    sys.path.append(path)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather.settings')
+
+activate_this = '/home/yourusername/.virtualenvs/myenv/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
+django.setup()
 
 application = get_wsgi_application()
 
